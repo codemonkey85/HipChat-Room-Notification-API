cc78c9ec9a49b4912565897a5bad8aaca8c3b5c1
Michael Bond <michael.bond.85@icloud.com>
2016-09-01 13:48:53 -0400

    Initial commit of project files

---
+++ HipChat Notification API/HipChatHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web.Script.Serialization;
+
+namespace HipChat_Notification_API
+{
+    public class HipChatHelper
+    {
+        public string ApiUrl { get; set; }
+        public string AuthToken { get; set; }
+
+        public HipChatHelper()
+        {
+            ApiUrl = string.Empty;
+            AuthToken = string.Empty;
+        }
+
+        public HipChatHelper(string apiUrl, string authToken)
+        {
+            ApiUrl = apiUrl;
+            AuthToken = authToken;
+        }
+
+        public bool SendNotification(string message, string roomId, string messageFormat = "text", string color = "green", bool notify = false)
+        {
+            string refResponse = null;
+            return SendNotification(message, roomId, messageFormat, color, notify, ref refResponse);
+        }
+
+        public bool SendNotification(string message, string roomId, string messageFormat, string color, bool notify, ref string response)
+        {
+            if (string.IsNullOrEmpty(ApiUrl.Trim()))
+                throw new Exception("API Url must not be empty!");
+            if (string.IsNullOrEmpty(AuthToken.Trim()))
+                throw new Exception("API Token must not be empty!");
+            if (string.IsNullOrEmpty(message.Trim()))
+                throw new Exception("Message must not be empty!");
+            if (string.IsNullOrEmpty(roomId.Trim()))
+                throw new Exception("Room Id must not be empty!");
+
+            if (response == null) response = string.Empty;
+            var serializer = new JavaScriptSerializer();
+            string json = serializer.Serialize(new
+            {
+                message,
+                message_format = messageFormat,
+                color,
+                notify
+            });
+            string webResponse = string.Empty;
+            return SendWebRequest(string.Format(ApiUrl, roomId, AuthToken), json, ref webResponse);
+        }
+
+        private bool SendWebRequest(string apiUrl, string json, ref string response)
+        {
+            var request = WebRequest.Create(apiUrl);
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            request.ContentLength = json.Length;
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(json);
+            }
+            var httpResponse = request.GetResponse() as HttpWebResponse;
+
+            if (httpResponse != null)
+                return false;
+
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                response = streamReader.ReadToEnd();
+                return true;
+            }
+        }
+    }
+}
---
+++ TestHipChatHelper/Program.cs
@@ -0,0 +1,35 @@
+﻿using HipChat_Notification_API;
+using System;
+
+namespace TestHipChatHelper
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                if (args == null || args.Length <= 4)
+                    throw new Exception("Must supply API Url, Auth Token, and Room Id as arguments!");
+
+                string apiUrl = args[1];
+                string authToken = args[2];
+                string roomId = args[3];
+
+                HipChatHelper hipChatHelper = new HipChatHelper
+                {
+                    ApiUrl = apiUrl,
+                    AuthToken = authToken
+                };
+
+                const string message = @"testing HipChatHelper";
+
+                hipChatHelper.SendNotification(message, roomId, notify: true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
